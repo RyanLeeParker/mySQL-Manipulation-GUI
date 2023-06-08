5534d2c2e60f4dc7a329b9d8744795df8ac0d144
RyanLeeParker <ryanleeparkeratx@gmail.com>
2023-06-08 05:46:05 -0500

    Second Test Push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        System.out.println("Mic check 1, 2, 1, 2");
+    }
+
+    public void Test_Push(ActionEvent actionEvent)
+    {
+        System.out.println("Button Pressed");
+    }
 }
