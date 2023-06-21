a7871e6e6e360020d24a1b42be2f9bc3329d7934
RyanLeeParker <ryanleeparkeratx@gmail.com>
2023-06-21 06:06:00 -0500

    Appointments view setup

--- src/controller/Appointments_Controller.java
+++ src/controller/Appointments_Controller.java
@@ -1,6 +1,40 @@
 package controller;
 
+import javafx.scene.control.*;
+
 public class Appointments_Controller
 {
 
+    public TextField Appt_ID_Input;
+    public TextField Appt_Name_Input;
+    public TextField Appt_Desc_Input;
+    public TextField Appt_Loc_Input;
+    public TextField Appt_Type_Input;
+    public TextField Appt_Cust_ID_Input;
+    public TextField Appt_UserID_Input;
+    public RadioButton Week_Radio;
+    public ToggleGroup Appt_Toggle;
+    public RadioButton Month_Radio;
+    public TableView Appointment_Table;
+    public TableColumn Appointment_ID_Column;
+    public TableColumn Appointment_Title_Column;
+    public TableColumn Appointment_Desc_Column;
+    public TableColumn Appt_Loc_Column;
+    public TableColumn Appt_Type_Column;
+    public TableColumn Appt_StartDateTime_Column;
+    public TableColumn Appt_EndDateTime_Coulmn;
+    public TableColumn Appt_CustID_Column;
+    public TableColumn Appt_ContactID_Column;
+    public TableColumn Appt_UserID_Column;
+    public Button AddButton;
+    public Button Edit_Button;
+    public Button DeleteButton;
+    public Button SaveButton;
+    public Button Cancel_Button;
+    public ComboBox Appointment_EndTime;
+    public ComboBox Appointment_Contact_CB;
+    public DatePicker Appt_StartDate_Picker;
+    public DatePicker Appt_EndDate_Picker;
+    public ComboBox Appointment_TimeStart_CB;
+    public RadioButton All_Radio;
 }
