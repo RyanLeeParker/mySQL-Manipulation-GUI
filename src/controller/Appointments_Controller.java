dd13f9770c3b58458063329f0e304610a5d19dfb
Gaius <ryanleeparkeratx@gmail.com>
2023-09-01 02:06:35 -0500

    Customer Controller done, excluding appointment functionality. Working on appointments del.

--- src/controller/Appointments_Controller.java
+++ src/controller/Appointments_Controller.java
@@ -1,14 +1,77 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import dao.Appointments_Access;
+import dao.Contacts_Access;
+import dao.Country_Access;
+import dao.Customer_Access;
+import dao.FirstLevelDivision_Access;
+import dao.Report_Access;
+import dao.Users_Access;
+import helper.JDBC;
+import helper.Time;
+import model.Appointments;
+import model.Contacts;
+import model.Country;
+import model.Customers;
+import model.First_Level_Division;
+import model.Reports;
+import model.Users;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import model.*;
+import dao.*;
+import views.Appointments.fxml;
+
+import dao.Appointments_Access;
+import dao.Country_Access;
+import dao.Customer_Access;
+import dao.FirstLevelDivision_Access;
+import helper.JDBC;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import helper.JDBC;
+import model.Customers;
+import model.*;
+import java.sql.Connection;
+import model.Country;
+
+//import static Main.timeUtil.convertTimeDateUTC;
 
 public class Appointments_Controller
 {
@@ -45,11 +108,26 @@ public class Appointments_Controller
     public ComboBox Appointment_TimeStart_CB;
     public RadioButton All_Radio;
 
-    public void initialize(URL url, ResourceBundle resourceBundle) throws Exception
+    public void initialize() throws Exception
     {
         //contact name assigned to appt using drop down menu or CB
         // appt id autogen
         // time should be stored UTC, but converted to local time for user      3 timezones: UTC, EST, SystemDefault
+
+        ObservableList<Appointments> allAppointmentsList = Appointments_Access.getAppointments();
+
+        Appointment_ID_Column.setCellValueFactory(new PropertyValueFactory<>("Appointment_ID"));
+        Appointment_Title_Column.setCellValueFactory(new PropertyValueFactory<>("Title"));
+        Appointment_Desc_Column.setCellValueFactory(new PropertyValueFactory<>("Description"));
+        Appt_Loc_Column.setCellValueFactory(new PropertyValueFactory<>("Location"));
+        Appt_Type_Column.setCellValueFactory(new PropertyValueFactory<>("Type"));
+        Appt_StartDateTime_Column.setCellValueFactory(new PropertyValueFactory<>("Start"));
+        Appt_EndDateTime_Coulmn.setCellValueFactory(new PropertyValueFactory<>("End"));
+        Appt_CustID_Column.setCellValueFactory(new PropertyValueFactory<>("Customer_ID"));
+        Appt_ContactID_Column.setCellValueFactory(new PropertyValueFactory<>("Contact_ID"));
+        Appt_UserID_Column.setCellValueFactory(new PropertyValueFactory<>("User_ID"));
+
+        Appointment_Table.setItems(allAppointmentsList);
 
     }
 
@@ -78,9 +156,52 @@ public class Appointments_Controller
 
     }
 
-    public void Delete_Button(ActionEvent actionEvent)
+    public void Delete_Button(ActionEvent actionEvent) throws Exception
     {
         //custom message displayed
+
+        try
+        {
+
+            //Customers Selected_Customer = (Customers) Customer_Table.getSelectionModel().getSelectedItem();
+            Customers Selected_Customer2 = (Customers) Customer_Table.getSelectionModel().getSelectedItem();
+
+            Connection connect = JDBC.openConnection();
+            int deleteAppointmentID = Appointment_Table.getSelectionModel().getSelectedItem().getAppointment_ID();
+            Appointments tempAppointment_1 = (Appointments)  Appointment_Table.getSelectionModel().getSelectedItem();
+            int Appt_to_del_1 = Selected_Customer.getCustomer_ID();
+
+
+
+            String deleteAppointmentType = Appointment_Table.getSelectionModel().getSelectedItem().getType();
+
+
+
+
+
+
+
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Delete the selected appointment with appointment id: " + deleteAppointmentID + " and appointment type " + deleteAppointmentType);
+
+            Optional<ButtonType> confirmation = alert.showAndWait();
+
+//            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);                            // use this one
+//            alert.setTitle("Delete Confirmation");
+//            alert.setContentText("Are you sure you want to delete this item?");
+//            alert.setHeaderText("Confirm Deletion");
+
+            if (confirmation.isPresent() && confirmation.get() == ButtonType.OK)
+            {
+                Appointments_Access.removeAppointment(deleteAppointmentID, connect);
+
+                ObservableList<Appointments> allAppointmentsList = Appointments_Access.getAppointments();
+                Appointment_Table.setItems(allAppointmentsList);
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void Save_Button(ActionEvent actionEvent)
